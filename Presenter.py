65177d8d65099c04321a8a3da25a6f111b0550ff
Ido Livne <livneido1@gmail.com>
2025-12-31 20:11:18 +0200

    feat(Presenter): introduced a presenter - this objects responsible to draw the image and the detections
    everything works together, need to adjust the video duration and use a multi process design

---
+++ Presenter.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from typing import List, Tuple
+
+import cv2
+import numpy as np
+
+
+class Presenter:
+    def __init__(self):
+        self._font = cv2.FONT_HERSHEY_SIMPLEX
+        self._line_type = cv2.LINE_AA
+        self._detection_color: Tuple[int, int, int] = (0, 255, 0)  # green
+        self._detection_line_thickness: int = 2
+        self._date_time_format: str = "%d-%m-%Y %H:%M:%S"
+        self._font_scale = 0.7
+        self._font_color = (0, 255, 255)
+        self._font_thickness = 2
+        self._date_position = (10, 30) # top left corner
+
+
+
+    def draw_and_show(self, image: np.ndarray, cnts: List[np.ndarray]):
+
+        self._draw_detections(cnts, image)
+
+        self._write_current_time(image)
+
+        cv2.imshow("Video With Detections", image)
+
+    def _write_current_time(self, image: np.ndarray):
+        timestamp = datetime.now().strftime(self._date_time_format)
+        cv2.putText(image, timestamp, self._date_position, self._font, self._font_scale, self._font_color, self._font_thickness, self._line_type)
+
+    def _draw_detections(self, cnts: list[np.ndarray], image: np.ndarray):
+        for c in cnts:
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(image, (x, y), (x + w, y + h), self._detection_color, self._detection_line_thickness)
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import cv2
+
 from Streamer import Streamer
 from Detector import Detector
+from Presenter import Presenter
 
 if __name__ == '__main__':
     streamer = Streamer(r'C:\Users\97250\Desktop\ido\axonVIsion\code\People - 6387.mp4')
     detector = Detector()
+    presenter = Presenter()
 
-    # yield_frames() returns the generator once
     images = streamer.yield_frames()
 
     for frame in images:
         original_frame, cnts = detector.detect(frame)
+        presenter.draw_and_show(original_frame, cnts)
+
         print(f"Detected {len(cnts)} moving objects")
+
+        # Windows os adaptions
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
