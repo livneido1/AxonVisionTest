a31480f3c519feb9997d5a56894b2adf2d8f69aa
Ido Livne <livneido1@gmail.com>
2025-12-31 20:31:43 +0200

    feat(Detection-multi-process): introduced the multi process design. used the mp queue due to its thread safe, efficiency, keeps the required process steps

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
+import multiprocessing as mp
 import cv2
-
 from Streamer import Streamer
 from Detector import Detector
 from Presenter import Presenter
 
-if __name__ == '__main__':
-    streamer = Streamer(r'C:\Users\97250\Desktop\ido\axonVIsion\code\People - 6387.mp4')
+
+def streamer_worker(path:str, produce_queue):
+    streamer = Streamer(path)
+    for frame in streamer.yield_frames():
+        produce_queue.put(frame)
+    # notifies for the end of the queue
+    produce_queue.put(None)
+
+
+def detector_worker(consume_queue, produce_queue):
     detector = Detector()
-    presenter = Presenter()
-
-    images = streamer.yield_frames()
-
-    for frame in images:
-        original_frame, cnts = detector.detect(frame)
-        presenter.draw_and_show(original_frame, cnts)
-
-        print(f"Detected {len(cnts)} moving objects")
-
-        # Windows os adaptions
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+    while True:
+        frame = consume_queue.get()
+        if frame is None:
             break
 
+        original_frame, cnts = detector.detect(frame)
+        produce_queue.put((original_frame, cnts))
+    # notifies for the end of the queue
+    produce_queue.put(None)
 
+
+def presenter_worker(consume_queue):
+    presenter = Presenter()
+    while True:
+        data = consume_queue.get()
+        if data is None:
+            break
+
+        image, cnts = data
+        presenter.draw_and_show(image, cnts)
+
+        # Windows os adaptations
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    video_path = r'C:\Users\97250\Desktop\ido\axonVIsion\code\People - 6387.mp4'
+
+    # common queues to communication between processes
+    frames_queue = mp.Queue(maxsize=30) # prevent streamer to overload and filling up the RAM
+    frames_with_detections_queue = mp.Queue(maxsize=30) # prevent the detector to overload and filling up the RAM
+
+    # defines the processes
+    streamer_proc = mp.Process(target=streamer_worker, args=(video_path, frames_queue))
+    detector_proc = mp.Process(target=detector_worker, args=(frames_queue, frames_with_detections_queue))
+    presenter_proc = mp.Process(target=presenter_worker, args=(frames_with_detections_queue,))
+
+    streamer_proc.start()
+    detector_proc.start()
+    presenter_proc.start()
+
+    streamer_proc.join()
+    detector_proc.join()
+    presenter_proc.join()
